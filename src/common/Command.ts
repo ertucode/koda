ad8342a69c4867d14bff41f07e2897d096c35f95
ertucode <ertugrulsirtt@gmail.com>
2026-01-20 01:50:46 +0300

    run command

--- src/common/Command.ts
+++ src/common/Command.ts
@@ -1,48 +1,62 @@
-import z from "zod";
+import z from 'zod'
 
 export const CommandParameterType = z
   .object({
-    type: z.enum(["string", "path"]),
+    type: z.literal('string'),
+    defaultValue: z.string().nullish(),
   })
   .or(
     z.object({
-      type: z.literal("select"),
+      type: z.literal('select'),
       options: z.array(
         z.object({
           label: z.string(),
           value: z.string(),
-        }),
+        })
       ),
-    }),
-  );
+      defaultValue: z.string().nullish(),
+    })
+  )
+  .or(
+    z.object({
+      type: z.literal('checkbox'),
+      defaultValue: z.boolean().nullish(),
+    })
+  )
+  .or(
+    z.object({
+      type: z.literal('path'),
+      defaultValue: z.string().nullish(),
+    })
+  )
 
 export const CommandMenuConfig = z.object({
-  placement: z.enum(["inline", "menu"]).default("menu"),
+  placement: z.enum(['inline', 'menu']).default('menu'),
   priority: z.number().default(0),
-});
-export type CommandMenuConfig = z.infer<typeof CommandMenuConfig>;
+})
+export type CommandMenuConfig = z.infer<typeof CommandMenuConfig>
 
 export const CommandParameter = z
   .object({
     name: z.string(),
     label: z.string().nullish(),
-    initialValue: z.string().nullish(),
+    optional: z.boolean().default(false),
   })
-  .and(CommandParameterType);
+  .and(CommandParameterType)
 
 export const CommandMetadata = z.object({
   name: z.string(),
   parameters: CommandParameter.array().nullish(),
   glob: z.string().nullish(),
   menu: CommandMenuConfig.nullish(),
-});
+})
 
-export type CommandMetadata = z.infer<typeof CommandMetadata>;
-export type CommandParameter = z.infer<typeof CommandParameter>;
+export type CommandMetadata = z.infer<typeof CommandMetadata>
+export type CommandParameter = z.infer<typeof CommandParameter>
 
 export const CommandReport = z.object({
-  type: z.literal("reload-path"),
+  type: z.literal('reload-path'),
   path: z.string(),
   fileToSelect: z.string().nullish(),
-});
-export type CommandReport = z.infer<typeof CommandReport>;
+})
+export type CommandReport = z.infer<typeof CommandReport>
