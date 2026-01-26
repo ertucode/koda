88e31458c1b588d04b7d45d2d4288483327ee149
ertucode <ertugrulsirtt@gmail.com>
2026-01-26 12:14:31 +0300

    es

--- src/common/file-size-too-large.ts
+++ src/common/file-size-too-large.ts
@@ -1,5 +1,5 @@
-import { FileCategory } from "./Contracts.js";
-import { getCategoryFromExtension } from "./file-category.js";
+import { FileCategory } from './Contracts.js'
+import { getCategoryFromExtension } from './file-category.js'
 
 export const fileSizeLimits: Partial<Record<FileCategory, number>> = {
   document: 1,
@@ -7,17 +7,18 @@ export const fileSizeLimits: Partial<Record<FileCategory, number>> = {
   video: Infinity,
   archive: 100,
   image: 100000,
-};
+  audio: Infinity,
+}
 
-const defaultLimit = 1;
+const defaultLimit = 1
 
 export function fileSizeTooLarge(extension: string, size: number) {
-  if (extension === "pdf") return { isTooLarge: false, limit: Infinity };
-  const category = getCategoryFromExtension(extension);
-  const limit = fileSizeLimits[category] ?? defaultLimit;
+  if (extension === 'pdf') return { isTooLarge: false, limit: Infinity }
+  const category = getCategoryFromExtension(extension)
+  const limit = fileSizeLimits[category] ?? defaultLimit
   // console.log(extension, size, limit * 1024 * 1024, size > limit * 1024 * 1024);
   return {
     isTooLarge: size > limit * 1024 * 1024,
     limit,
-  };
+  }
 }
