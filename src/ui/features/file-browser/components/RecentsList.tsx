c16a6552e0780f12584c374713b02668a9739603
ertucode <ertugrulsirtt@gmail.com>
2026-02-09 12:30:54 +0300

    yes

--- src/ui/features/file-browser/components/RecentsList.tsx
+++ src/ui/features/file-browser/components/RecentsList.tsx
@@ -1,59 +1,49 @@
-import { FileIcon, FolderIcon } from "lucide-react";
-import { useSelector } from "@xstate/store/react";
-import { type RecentItem, recentsStore, selectRecents } from "../recents";
-import { FileBrowserSidebarSection } from "./FileBrowserSidebarSection";
-import { getWindowElectron } from "@/getWindowElectron";
-import {
-  directoryStore,
-  directoryHelpers,
-  selectDirectory,
-} from "../directoryStore/directory";
+import { FileIcon, FolderIcon } from 'lucide-react'
+import { useSelector } from '@xstate/store/react'
+import { type RecentItem, recentsStore, selectRecents } from '../recents'
+import { FileBrowserSidebarSection } from './FileBrowserSidebarSection'
+import { getWindowElectron } from '@/getWindowElectron'
+import { directoryStore, directoryHelpers, selectDirectory } from '../directoryStore/directory'
 
 interface RecentsListProps {
-  className?: string;
+  className?: string
 }
 
 export function RecentsList({ className }: RecentsListProps) {
-  const r = useSelector(recentsStore, selectRecents);
-  const activeDirectoryId = useSelector(
-    directoryStore,
-    (s) => s.context.activeDirectoryId,
-  );
-  const directory = useSelector(
-    directoryStore,
-    selectDirectory(activeDirectoryId),
-  );
+  const r = useSelector(recentsStore, selectRecents)
+  const activeDirectoryId = useSelector(directoryStore, s => s.context.activeDirectoryId)
+  const directory = useSelector(directoryStore, selectDirectory(activeDirectoryId))
 
   return (
     <FileBrowserSidebarSection
       items={r}
       emptyMessage="No recent directories"
-      getKey={(recent) => recent.fullPath}
-      isSelected={(recent) =>
-        directory.type === "path" && directory.fullPath === recent.fullPath
-      }
-      onClick={(recent) => {
-        if (recent.type === "dir") {
-          directoryHelpers.cdFull(recent.fullPath, activeDirectoryId);
+      getKey={recent => recent.fullPath}
+      isSelected={recent => directory.type === 'path' && directory.fullPath === recent.fullPath}
+      onClick={recent => {
+        if (recent.type === 'dir') {
+          directoryHelpers.cdFull(recent.fullPath, activeDirectoryId)
         } else {
-          getWindowElectron().openFile(recent.fullPath);
+          getWindowElectron().openFile(recent.fullPath)
         }
       }}
       className={className}
-      render={(recent) => (
+      render={recent => (
         <>
-          {recent.type === "dir" ? (
+          {recent.type === 'dir' ? (
             <FolderIcon className="size-4 min-w-4 text-blue-500" />
           ) : (
             <FileIcon className="size-4 min-w-4 text-green-500" />
           )}
-          <span className="truncate">{recentName(recent)}</span>
+          <span className="truncate" title={recent.fullPath}>
+            {recentName(recent)}
+          </span>
         </>
       )}
     />
-  );
+  )
 }
 
 function recentName(recent: RecentItem) {
-  return recent.fullPath.split("/").pop() || recent.fullPath;
+  return recent.fullPath.split('/').pop() || recent.fullPath
 }
